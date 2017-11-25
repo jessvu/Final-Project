9bd285a2d61dcfb0ac381c2447e6c0b6ac16a6b5
Jess <jessvu@umich.edu>
2017-11-25 14:49:44 -0500

    first commit

---
+++ insta.py
@@ -0,0 +1,4 @@
+from instagram.client import InstagramAPI
+
+api = InstagramAPI(client_id = '19a9f427686348c7b101de72738ba569', client_secret =' 9e8b768924f94e3088208d2a7723dd89')
+popular_media = api.media_popular(count=100)
